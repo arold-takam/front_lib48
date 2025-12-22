6f144b59dd6b7e1b4e7c55962e4cff2b96be9446
arold-takam <www.aroldtakam3@gmail.com>
2025-12-22 15:44:41 +0100

    starting connection with back

---
+++ adminPanel/js/displayBook.js
@@ -0,0 +1,44 @@
+async function fetchAndDisplayBooks() {
+    const container = document.querySelector('.panel .panelDown');
+    console.log(container)
+
+    // Identifiants de test (ceux que tu as mis dans Postman)
+    const name = "toto@gmail.com";
+    const password = "toto237";
+    // Encodage en Base64 pour le header Authorization
+    const auth = btoa(`${name}:${password}`);
+
+    try {
+        const response = await fetch(`${CONFIG.API_URL}/books/get/All`, {
+            method: 'GET',
+            headers: {
+                'Authorization': `Basic ${auth}`,
+            }
+        });
+
+        if (!response.ok) throw new Error("Erreur " + response.status);
+
+        const books = await response.json();
+        container.innerHTML = "";
+
+        books.forEach(book => {
+            const li = document.createElement('li');
+            li.className = 'bookLine';
+            li.innerHTML = `
+                <ul class="infoLine">
+                    <li><a href="../html/aDashBookDetails.html?id=${book.id}">${book.titre}</a></li>
+                    <li>${book.auteur}</li>
+                    <li>${book.etatLivre}</li>
+                    <li>${book.estDisponible ? 'DISPONIBLE' : 'EMPRUNTÉ'}</li>
+                </ul>
+            `;
+            container.appendChild(li);
+        });
+
+    } catch (error) {
+        console.error("Erreur:", error);
+        container.innerHTML = `<p style='padding:20px; color:red; font-size: x-large;'>Accès refusé ou serveur éteint.</p>`;
+    }
+}
+
+document.addEventListener('DOMContentLoaded', fetchAndDisplayBooks);
---
+++ adminPanel/js/loadBookDetails.js
@@ -0,0 +1,59 @@
+const params = new URLSearchParams(window.location.search);
+const bookID = params.get("id");
+
+console.log("Book ID = " + bookID);
+
+const name = "toto@gmail.com";
+const password = "toto237";
+const auth = btoa(`${name}:${password}`);
+
+async function loadBookDetails() {
+    try {
+        const response = await fetch(`${CONFIG.API_URL}/books/get/byID/${bookID}`,{
+            method: 'GET',
+            headers: {
+                'Authorization': `Basic ${auth}`,
+            }
+        });
+
+        if (!response.ok) throw new Error("Erreur: "+response.status);
+
+        const book = await response.json();
+        displayBookDetails(book);
+    }catch(err) {
+        console.error("Erreur: ", err);
+        document.querySelector('.bookCard').innerHTML = `<p style = 'padding: 20px; color: red; font-size: x-large;'>Impossible de charger les détails du livre.</p>`;
+    }
+}
+
+function displayBookDetails(book) {
+    const card = document.querySelector('.bookCard .card');
+    card.style.background = `url(${book.urlCoverImage}) center no-repeat || url("../ressources/images/fondMenu.jpg") center no-repeat`;
+    card.style.backgroundSize = "cover";
+
+    const infoList = document.querySelector('.bookCard .groupInfo');
+    infoList.innerHTML = `
+        <li>
+            <p>Catégorie :</p>
+            <b>${book.category?.name || 'N/A'}</b>
+        </li>
+        <li>
+            <p>Auteur :</p>
+            <b>${book.auteur}</b>
+        </li>
+        <li>
+            <p>Éditeur :</p>
+            <b>${book.editeur}</b>
+        </li>
+        <li>
+            <p>État du livre :</p>
+            <b>${book.etatLivre}</b>
+        </li>
+        <li>
+            <p>Disponibilité :</p>
+            <b>${book.estDisponible ? 'DISPONIBLE' : 'EMPRUNTE'}</b>
+        </li>
+    `;
+}
+
+document.addEventListener('DOMContentLoaded', loadBookDetails);
